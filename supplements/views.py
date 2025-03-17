0bcd467f5506832aee88dceabf12fcd2ece9da3e
tomechava <techavarrg@eafit.edu.co>
2025-03-16 19:16:07 -0500

    about us

--- supplements/views.py
+++ supplements/views.py
@@ -3,7 +3,32 @@ from django.views import View
 
 # Create your views here.
 class HomePageView(View):
-    template_name = "supplements/home.html"
+    template_name = "home.html"
     
     def get(self, request):
         return render(request, self.template_name)
+    
+class AboutView(View):
+    template_name = "about.html"
+    
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class ProductIndexView(View):
+    template_name = "product_index.html"
+    
+    def get(self, request):
+        return render(request, self.template_name)
+
+class ProductView(View):
+    template_name = "product.html"
+    
+    def get(self, request):
+        return render(request, self.template_name)
+
+class ProductEditView(View):
+    template_name = "products.html"
+    
+    def get(self, request):
+        return render(request, self.template_name)
+
